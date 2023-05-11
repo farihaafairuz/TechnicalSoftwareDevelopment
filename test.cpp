a76999556a8b1b6a25d0eb432a41bda430752138
AlexSecoulidis <133176627+AlexSecoulidis@users.noreply.github.com>
2023-05-11 12:14:48 +1000

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,23 @@
+//test file 
+
+#include <iostream> 
+using namespace std;
+
+string menu(){
+cout<<"Select an option below by entering a letter (example: P)\n";
+cout<<"P - Print numbers for a particular candidate\n";
+cout<<"A - Add number of votes to a candidate\n";
+cout<<"S - Display the smallest number of votes candidate\n";
+cout<<"L - Display the largest number of votes candidate\n";
+cout<<"Q - Quit\n";
+}
+    
+
+
+
+
+int main(){
+menu();
+
+return 0;
+}
