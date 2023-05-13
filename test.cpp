855bb9adda95ea96d2f48f10e59292b43b1478eb
Jenny Nguyen <45861728+jenny-nn@users.noreply.github.com>
2023-05-13 21:58:14 +1000

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,31 +1,60 @@
-//test file 
+//test file
 
-#include <iostream> 
+#include <iostream>
 #include <fstream>//libary for opening external files with ifstream and ofstream
 #include <cstdlib>//needed for closing the external files
 using namespace std;
 
-string menu(){//a prototype function for displaying the menu and allowed commands
-cout<<"Select an option below by entering a letter (example: P)\n";
-cout<<"P - Print numbers for a particular candidate\n";
-cout<<"A - Add number of votes to a candidate\n";
-cout<<"S - Display the smallest number of votes candidate\n";
-cout<<"L - Display the largest number of votes candidate\n";
-cout<<"Q - Quit\n";
+void print_menu() {
+    cout << "Select an option below by entering a letter (example: P)\n";
+    cout << "P - Print numbers for a particular candidate\n";
+    cout << "A - Add number of votes to a candidate\n";
+    cout << "S - Display the smallest number of votes candidate\n";
+    cout << "L - Display the largest number of votes candidate\n";
+    cout << "Q - Quit\n";
+    cout << "Enter your option: ";
 }
 
-char P(){
-    char p;
-    cout<<"P: enter a canadidate id\n"
-    if(cin>>p;){
-        ifstream infile;
-        infile.open("candidates.txt");//opens candidate file
-        
+char menu() {
+    char selected;
+    print_menu();
+
+    cin >> selected;
+    selected = toupper(selected);
+    cout << endl;
+
+    if (selected != 'P' && selected != 'A' && selected != 'S' && selected != 'L' && selected != 'Q') {
+    cout << "Unknown Selection, Please Try Again\n" << endl;
+    menu();
+ }
+
+    return selected;
+}
+
+int main() {
+    while (true) {
+        char selected;
+        selected = menu();
+
+        switch (selected)
+        {
+    //    case 'P':
+    //        print_candidate_numbers();
+    //      break;
+    //    case 'A':
+    //      add_votes_to_candidate();
+    //      break;
+    //    case 'S':
+    //      display_lowest_candidate();
+    //      break;
+    //    case 'L':
+    //      display_highest_candidate();
+    //      break;
+    //    case 'Q':
+    //      exit(0);
+        default:
+          menu();
+        }
     }
-}
 
-int main(){
-menu();
-
-return 0;
 }
