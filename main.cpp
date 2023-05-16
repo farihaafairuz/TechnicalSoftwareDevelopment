bb1e6917196a5fa73d3332e35123efffacec6e2a
AlexSecoulidis <133176627+AlexSecoulidis@users.noreply.github.com>
2023-05-16 22:54:42 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-////test file
-//
+//test file
+
 #include <iostream> //header file for lots of C++ functions
 #include <fstream> //libary for opening external files with ifstream and ofstream
 #include <sstream> //header file for classes
@@ -17,7 +17,7 @@ void selection(vector<vector<string>> candidates);
 void print_candidates(vector<vector<string>> candidates);
 vector<vector<string>> add_votes_to_candidate(vector<vector<string>> candidates);
 void display_smallest_number_of_votes(vector<vector<string>> candidates);
-//void display_largest_number_of_votes(vector<vector<string>> candidates)s;
+void display_largest_number_of_votes(vector<vector<string>> candidates);
 
 //const string VOTER_FILE = "voters.txt";
 //const string CANDIDATE_FILE = "candidates.txt";
@@ -104,7 +104,8 @@ void print_candidate_numbers(vector<vector<string>> candidates)
     cout << "Candidate " << selected << ": " << candidates[selected - 1][1] << endl;
     cout << "Votes: " << candidates[selected - 1][4] << endl << endl;
     cin.ignore();//used with cin.get() to add a pause until the user enters "enter" 
-    cin.get();//because the program would just go straight back to the "menu" without displaying the information clearly
+    cin.get();//because the program would display the "menu" making the desired output information above the menu 
+
 }
 
 
@@ -148,7 +149,7 @@ void write_candidates_to_file(vector<vector<string>> candidates) {
 
 //display smallest number of votes
 void display_smallest_number_of_votes(vector<vector<string>> candidates) {
-    int smallest = stoi(candidates[0][4]);
+    int smallest = stoi(candidates[0][4]);//stoi coverts string to interger
     string candidate_number = candidates[0][0];
     string candidate_name = candidates[0][1];
     for (int i = 1; i < candidates.size(); i++) {
@@ -159,6 +160,21 @@ void display_smallest_number_of_votes(vector<vector<string>> candidates) {
         }
     }
     cout << "Candidate " << candidate_number << " " << candidate_name<< " has the smallest number of votes: " << smallest << endl << endl;
+}
+//display largest number of votes
+void display_largest_number_of_votes(vector<vector<string>> candidates){
+    int largest = stoi(candidates[0][4]);
+    string candidate_number = candidates[0][0];//declares the candidate number to be at [0][0] in the array of the candidate.txt file
+    string candidate_name = candidates[0][1];//declares the candidate name to be at [0][1] in the array of the candidate.txt file
+    for (int i = 1; i < candidates.size(); i++) {//for loop to cycle through candidate.txt file information
+        if (stoi(candidates[i][4]) > largest) {
+            largest = stoi(candidates[i][4]);
+            candidate_number = candidates[i][0];
+            candidate_name = candidates[i][1];
+        }
+        
+    }
+    cout<<"Candidate " <<candidate_number << " " << candidate_name << " has the largest number of votes: " << largest << endl << endl;
 }
 
 // add votes to candidate
@@ -194,11 +210,15 @@ vector<vector<string>> add_votes_to_candidate(vector<vector<string>> candidates)
 
 
 void selection(vector<vector<string>> candidates){
+/*
+function takes users input and checks it against the if else statements
+
+*/
     char selected;
     cin >> selected;
     cout << endl;
-    while (toupper(selected) != 'Q') {
-        if (toupper(selected) == 'P') {
+    while (toupper(selected) != 'Q') {//checks to see if Q has not been entered and continues down the if else statements
+        if (toupper(selected) == 'P') {//toupper coverts outputs to be uppercase 
             if (!candidates.empty()) {
                 print_candidate_numbers(candidates);
             }
@@ -225,7 +245,7 @@ void selection(vector<vector<string>> candidates){
         }
         else if (toupper(selected) == 'L') {
             if (!candidates.empty()) {
-//                display_largest_number_of_votes(candidates);
+                display_largest_number_of_votes(candidates);
             }
             else {
                 cout << "The list is empty. No candidates.\n" << endl;
@@ -241,7 +261,7 @@ void selection(vector<vector<string>> candidates){
     cout << "Goodbye!\n" << endl;
 }
 // Print Menu Options
-void print_menu() {
+void print_menu() {//prototype of the menu to be displayed to the user
     cout << "Select an option below by entering a letter (example: P)\n";
     cout << "P - Print numbers for a particular candidate\n";
     cout << "A - Add number of votes to a candidate\n";
@@ -258,9 +278,9 @@ int main(){
     voters = read_voters_from_file();
     candidates = read_candidates_from_file();
     
-    print_all_candidates(candidates);
+    print_all_candidates(candidates);//calls function that prints the candidates
     cout << endl;
-    print_menu();
+    print_menu();//calls the menu function 
     selection(candidates);
 
     return 0;
